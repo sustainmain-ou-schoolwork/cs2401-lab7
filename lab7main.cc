f873a8ec4733e97b87b84b70636fe45838acd835
Josh Marusek <jm553020@ohio.edu>
2022-03-01 16:02:39 -0500

    download project files

---
+++ lab7main.cc
@@ -0,0 +1,52 @@
+/*************************************************************************
+    This is the main for the Lab 7 Assignment of CS2401. You will need 
+to implement the rearview function which should display the list 
+backwards. Also, there are problems in the code which you will need to 
+diagnose using gdb.
+Finally you will also need to write and test the copy constructor for 
+this class.
+	John Dolan	School of EECS Ohio University, Athens Ohio
+			Spring 2018
+    Patricia Lindner    October 2021
+*************************************************************************/
+
+#include <iostream>
+#include <cstdlib>
+#include "llist.h"
+using namespace std;
+
+int main(){
+
+    int n;
+    LList somenums;
+    for(int i = 0; i < 35; ++i)
+    {
+        n = rand() % 700 + 1;
+	    somenums.add_item(n);
+    }
+
+// Once you have written the rear-view this should let you
+// see the list frontwards and backwards.
+    cout << "Front View:\n";
+    somenums.frontview();
+    cout << "\nRear View:\n";
+    somenums.rearview();
+
+// This part will be uncommented once you have written the copy constructor
+/*
+{
+    LList numcopy(somenums);  // call to the copy constructor
+    cout << "Copied List Front View:\n";
+    numcopy.frontview();
+    cout << "\nCopied List Rear View:\n";
+    numcopy.rearview();
+}   // Line 42 - What happens here?
+
+// Checking the original list
+    cout << "Original List Front View:\n";
+    somenums.frontview();
+    cout << "\nOriginal List Rear View:\n";
+    somenums.rearview();
+*/
+return 0;
+}
---
+++ llist.cc
@@ -0,0 +1,73 @@
+/************************************************************************
+     This is the implementation file for the LList class which is a 
+container based on a doubly linked list. The student is required to 
+write the rearview function as well as the copy constructor. Note that 
+the copy constructor will not be able to use the add function to produce 
+a true copy because of the strange behavior of the add function, which 
+treats all numbers divisible by five differently than other numbers.
+	John Dolan		Ohio University		Spring 2018
+************************************************************************/
+#include <iostream>
+#include "llist.h"
+using namespace std;
+
+// node constructor
+node::node(int d, node* p, node* n){
+	data_field = d;
+	nextptr = n;
+	previousptr = p;
+}
+
+
+// Llist functions
+LList::LList(){
+	head = tail = NULL;
+	nodecount = 0;
+}
+
+LList::~LList(){
+	node* rmptr;
+	while(head != NULL){
+		rmptr = head;
+		head = head -> next();
+		delete rmptr;
+	}
+}
+
+void LList::frontview()const{
+    node* cursor = head;
+    while (cursor != NULL){
+		cout << cursor -> data() << "  ";
+		cursor = cursor -> next();
+    }
+	cout << endl;
+}
+
+// The student is required to write the implementation of this function
+void LList::rearview()const{
+
+
+}
+
+
+void LList::add_item(int item){
+    if(head == NULL){
+		head = tail = new node(item);
+		nodecount++;
+    }
+    else if(item % 5 == 0 && nodecount > 3){
+		int i = 0;
+		node* cursor = head;
+		while(i < nodecount / 2){
+			cursor = cursor -> next();
+			i++;
+		}
+		cursor -> previous() -> set_next(new node(item, cursor -> next(), cursor -> previous()));
+		nodecount++;
+    }
+    else{
+		tail -> set_next(new node(item, tail));
+		tail = tail -> next();
+		nodecount++;
+    }
+}
